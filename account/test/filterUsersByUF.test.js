67a0f971a7cd0d325d4a20bf2647f1b573c74885
leticia <48692890+monteiroleticia@users.noreply.github.com>
2023-01-05 13:49:21 -0300

    feat: filter users by UF

---
+++ account/test/filterUsersByUF.test.js
@@ -0,0 +1,5 @@
+import {usersArray} from "../src/mockUsers.js"; 
+import {filterUsersByUFUseCase} from "../src/use-case/filterUsersByUF.js";
+
+console.log(filterUsersByUFUseCase('SP'));
+console.log(filterUsersByUFUseCase('RJ'));
