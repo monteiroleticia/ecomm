34e580124fcabea2f15bbfc1076228e9fe6ae25d
leticia <48692890+monteiroleticia@users.noreply.github.com>
2023-02-07 09:13:53 -0300

    fix: changing docs / to api-docs

--- account/src/routes/index.js
+++ account/src/routes/index.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import accounts from './accountsRoutes.js';
 import swaggerUi from 'swagger-ui-express';
-import swaggerDocument from '../../swagger/account.json' assert {type: 'json'};
+import swaggerDocument from '../../swagger/account.json'; assert {type: 'json'};
 
 const routes = (app) => {
-  app.use('/api/docs', swaggerUi.serve)
-     .get('/api/docs', swaggerUi.setup(swaggerDocument));
+  app.use('/api-docs', swaggerUi.serve)
+     .get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 app.use(
     express.json(),
--- product/src/routes/index.js
+++ product/src/routes/index.js
@@ -5,8 +5,8 @@ import swaggerUi from 'swagger-ui-express';
 import swaggerDocument from '../../swagger/product.json' assert {type: 'json'};
 
 const routes = (app) => {
-  app.use('/api/docs', swaggerUi.serve)
-     .get('/api/docs', swaggerUi.setup(swaggerDocument));
+  app.use('/api-docs', swaggerUi.serve)
+     .get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 app.use(
     express.json(),
