a38f39cc93ef5b8fd63b03f86fce87e784152785
leticia <48692890+monteiroleticia@users.noreply.github.com>
2023-02-09 12:36:46 -0300

    fix: update node version + solve mongo connection issues

--- account/src/routes/index.js
+++ account/src/routes/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import accounts from './accountsRoutes.js';
 import swaggerUi from 'swagger-ui-express';
-import swaggerDocument from '../../swagger/account.json'; assert {type: 'json'};
+import swaggerDocument from '../../swagger/account.json' assert {type: 'json'};
 
 const routes = (app) => {
   app.use('/api-docs', swaggerUi.serve)
