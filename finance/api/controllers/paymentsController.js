e5566dfd2d36cab3d95bf91672f5715fd55aecbc
monteiroleticia <mntrltc@gmail.com>
2023-02-10 22:11:21 -0300

    feat: update payment status on the finance api

--- finance/api/controllers/paymentsController.js
+++ finance/api/controllers/paymentsController.js
@@ -6,7 +6,7 @@ const db = require('../models');
       const payment = {...req.body, status: 'CRIADO'}
       try {
         const {id, status} = await db.Payments.create(payment);
-        return res.status(201).json({id, status});
+        return res.status(201).set('Location', `/payments/${id}`).json({id, status});
       } catch (error) {
         return res.status(500).json(error.message)
       }
@@ -16,7 +16,7 @@ const db = require('../models');
       const {id} = req.params;
       try {
         const payment = await db.Payments.findOne({
-          where: { id: Number(id) },
+          where: {id: Number(id)},
           attributes: ['id', 'amount', 'cardholder', 'cardNumber', 'expDate', 'status', 'createdAt', 'updatedAt']
         })
         return res.status(200).json(payment)
@@ -24,6 +24,16 @@ const db = require('../models');
         return res.status(500).json(error.message)
       }
     }
+
+    static async updateStatus(req, res) {
+      const {id} = req.params; 
+      try {
+        await db.Payments.update(req.body, {where: {id: Number(id)}});
+        const payment = await db.Payments.findOne( {where: {id: Number(id)}});
+        return res.status(200).json(payment);
+      } catch (error) {
+        return res.status(500).json(error.message);
+    }}
  }
 
  module.exports = PaymentsController;
--- finance/api/routes/paymentsRoutes.js
+++ finance/api/routes/paymentsRoutes.js
@@ -6,5 +6,6 @@ const router = Router();
 router
     .post('/payments', PaymentsController.submitPayment)
     .get('/payments/:id', PaymentsController.getPayment)
-    
+    .patch('/payments/:id', PaymentsController.updateStatus)
+
 module.exports = router;
