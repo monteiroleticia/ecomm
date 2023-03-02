2e081d15ec42296f43e147248d947c26d609038e
leticia <48692890+monteiroleticia@users.noreply.github.com>
2023-03-01 23:15:18 -0300

    refactor: ESLint fixes for order api

--- order/server.js
+++ order/server.js
@@ -1,9 +1,7 @@
-import app from './src/app.js'
+import app from './src/app.js';
 
 const port = process.env.PORT || 3003;
 
-
 app.listen(port, () => {
-  console.log(`Servidor escutando em http://localhost:${port}`)
-})
-
+  console.log(`Servidor escutando em http://localhost:${port}`);
+});
