991949f31863c0934553a40a410d52dfb1d0a432
leticia <48692890+monteiroleticia@users.noreply.github.com>
2023-03-01 22:21:05 -0300

    refactor: ESLint fixes for finance api

--- finance/test/paymentsRoutes.test.js
+++ finance/test/paymentsRoutes.test.js
@@ -4,22 +4,22 @@ const app = require('../api/app.js');
 
 let id;
 describe('POST /api/admin/payments', () => {
-    it('should submit a payment', async () => {
-        const response = await request(app)
-            .post('/api/admin/payments')
-            .send({           
-              amount: 6300,
-              cardholder: 'Aparecida Ribeiro',
-              cardNumber: '5468035026113019',
-              expDate: '2024-08',
-              cvv: '518'
-            })
-            .expect('content-type', /json/)
-            .expect(201);
+  it('should submit a payment', async () => {
+    const response = await request(app)
+      .post('/api/admin/payments')
+      .send({
+        amount: 6300,
+        cardholder: 'Aparecida Ribeiro',
+        cardNumber: '5468035026113019',
+        expDate: '2024-08',
+        cvv: '518',
+      })
+      .expect('content-type', /json/)
+      .expect(201);
 
-        id = response.body.id;
-    })
-})
+    id = response.body.id;
+  });
+});
 
 describe('GET /api/admin/payments/:id', () => {
   it('should return a payment found by its id', async () => {
@@ -28,8 +28,8 @@ describe('GET /api/admin/payments/:id', () => {
       .set('Accept', 'application/json')
       .expect('content-type', /json/)
       .expect(200);
-  })
-})
+  });
+});
 
 describe('PATCH /api/admin/payments/:id', () => {
   it('should update a payment status', async () => {
@@ -39,7 +39,7 @@ describe('PATCH /api/admin/payments/:id', () => {
       .set('Accept', 'application/json')
       .expect('content-type', /json/)
       .expect(200);
-  })
+  });
 
   it('shouldnt confirm a cancelled payment', async () => {
     await request(app)
@@ -48,7 +48,5 @@ describe('PATCH /api/admin/payments/:id', () => {
       .set('Accept', 'application/json')
       .expect('content-type', /json/)
       .expect(400);
-  })
-
-})
-
+  });
+});
