b8b570ad3efae49b50fa593bf987573c0e55862a
leticia <48692890+monteiroleticia@users.noreply.github.com>
2023-03-16 09:19:22 -0300

    fix: import swagger without assert

--- account/src/routes/index.js
+++ account/src/routes/index.js
@@ -1,12 +1,14 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import { createRequire } from 'node:module';
 import accounts from './accountsRoutes.js';
 
-/* import swaggerUi from 'swagger-ui-express';
-import swaggerDocument from '../../swagger/account.json' assert {type: 'json'}; */
+const require = createRequire(import.meta.url);
+const swaggerDocument = require('../../swagger/account.json');
 
 const routes = (app) => {
-  /* app.use('/api-docs', swaggerUi.serve)
-    .get('/api-docs', swaggerUi.setup(swaggerDocument)); */
+  app.use('/api-docs', swaggerUi.serve)
+    .get('/api-docs', swaggerUi.setup(swaggerDocument));
 
   app.use(
     express.json(),
--- product/src/routes/index.js
+++ product/src/routes/index.js
@@ -1,13 +1,15 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import { createRequire } from 'node:module';
 import categories from './categoriesRoutes.js';
 import products from './productsRoutes.js';
-/* import swaggerUi from 'swagger-ui-express';
-import swaggerDocument from '../../swagger/product.json' assert {type: 'json'}; */
+
+const require = createRequire(import.meta.url);
+const swaggerDocument = require('../../swagger/product.json');
 
 const routes = (app) => {
-  /* app.use('/api-docs', swaggerUi.serve)
-     .get('/api-docs', swaggerUi.setup(swaggerDocument)); */
-
+  app.use('/api-docs', swaggerUi.serve)
+    .get('/api-docs', swaggerUi.setup(swaggerDocument));
   app.use(
     express.json(),
     categories,
